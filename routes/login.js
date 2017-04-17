e076ec55eec916536705ebc728af8d716b91f15c
gxx <562606139@qq.com>
2017-04-18 00:03:43 +0800

    4.18

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,32 @@
-module.exports = function ( app ) {
-    app.get('/login',function(req,res){
+module.exports = function (app) {
+    app.get('/login', function (req, res) {
         res.render('login');
     });
 
     app.post('/login', function (req, res) {
         var User = global.dbHelper.getModel('user'),
             uname = req.body.uname;
-        User.findOne({name: uname}, function (error, doc) {
+        User.findOne({
+            name: uname
+        }, function (error, doc) {
             if (error) {
-                res.send(500);
-                console.log(error);
+                res.status(500).json({
+                    error: error
+                })
             } else if (!doc) {
                 req.session.error = '用户名不存在！';
-                res.send(404);
+                res.status(404).json({
+                    error: '用户名不存在！'
+                })
+                // res.send(404);
             } else {
-               if(req.body.upwd != doc.password){
-                   req.session.error = "密码错误!";
-                   res.send(404);
-               }else{
-                   req.session.user=doc;
-                   res.send(200);
-               }
+                if (req.body.upwd != doc.password) {
+                    req.session.error = "密码错误!";
+                    res.status(404)
+                } else {
+                    req.session.user = doc;
+                    res.send(200);
+                }
             }
         });
     });
--- routes/register.js
+++ routes/register.js
@@ -1,28 +1,34 @@
 // app：express对象
-module.exports = function ( app ) {
-  app.get('/register', function(req, res) {
-      res.render('register');
-  });
-  app.post('/register', function (req, res) {
-     var User = global.dbHelper.getModel('user'),
-     uname = req.body.uname;
-     User.findOne({name: uname}, function (error, doc) {
-       if (doc) {
-            req.session.error = '用户名已存在！';
-            res.send(500);
-        } else {
-            User.create({
-                name: uname,
-                password: req.body.upwd
-            }, function (error, doc) {
-                if (error) {
-                    res.send(500);
-                } else {
-                    req.session.error = '用户名创建成功！';
-                    res.send(200);
-                }
-            });
-        }
+module.exports = function (app) {
+    app.get('/register', function (req, res) {
+        res.render('register');
     });
-  });
+    app.post('/register', function (req, res) {
+        var User = global.dbHelper.getModel('user'),
+            uname = req.body.uname;
+        User.findOne({
+            name: uname
+        }, function (error, doc) {
+            if (doc) {
+                req.session.error = '用户名已存在！';
+                res.status(500).json({
+                    error: error
+                })
+            } else {
+                User.create({
+                    name: uname,
+                    password: req.body.upwd
+                }, function (error, doc) {
+                    if (error) {
+                        res.status(500).json({
+                            error: error
+                        })
+                    } else {
+                        req.session.error = '用户名创建成功！';
+                        res.send(200);
+                    }
+                });
+            }
+        });
+    });
 }
